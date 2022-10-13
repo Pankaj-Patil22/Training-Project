d36f9a99de072cfbf25005a1dbcaef5d5f5df9e5
KARTIK10BIN <kartik240934@gmail.com>
2022-10-13 10:01:42 +0530

    tables day3 additional features date and time

--- FrontEnd/JS/table.js
+++ FrontEnd/JS/table.js
@@ -11,6 +11,9 @@ function handleChange(checkbox) {
         // document.getElementById("submit").setAttribute("disabled", "disabled");
    }
    console.log(yourGlobalVariable)
+   if(yourGlobalVariable.length != 0){
+    
+   }
 }
 
 function getAvailableTables(){
@@ -29,4 +32,58 @@ function getAvailableTables(){
          }
         })
     })
+}
+
+function getDateMin(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    } 
+    
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("datefield").setAttribute("min", today);
+}
+
+function getAvailableTimmings(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    } 
+    
+    today = yyyy + '-' + mm + '-' + dd;
+
+    const notes = document.querySelectorAll('.time');
+
+    if(document.getElementById("datefield").value == today){
+        notes.forEach((note) => {
+            if(hour < note.classList[1]){
+                document.getElementById("time-"+note.classList[1]).removeAttribute('disabled','')
+            }
+            else if(hour >= note.classList[1]){
+                document.getElementById("time-"+note.classList[1]).setAttribute('disabled','')
+            }
+        });
+    }
+    else{
+        notes.forEach((note) => {
+            document.getElementById("time-"+note.classList[1]).removeAttribute('disabled','')
+        });
+    }
 }
